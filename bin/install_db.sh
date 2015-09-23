753561ce743f786570b3b073d9f292cd2aca6d59
Federico <fthiella@gmail.com>
2015-09-23 10:55:13 +0200

    Create install_db.sh

---
+++ bin/install_db.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/sh
+
+if [ -f data/sentosa.db ];
+then
+  echo "Removing old sentosa.db ..."
+  rm data/sentosa.db
+fi
+
+echo "Importing sentosa.sql ..."
+sqlite3 -batch data/sentosa.db < db/sentosa.sql
+
+if [ -f db/sentosa.custom.sql ];
+then
+  echo "Importing sentosa.custom.sql ..."
+  sqlite3 -batch data/sentosa.db < db/sentosa.custom.sql
+fi
+
+if [ -f data/chinook.db ];
+then
+  echo "Removing old chinook.db ..."
+  rm data/chinook.db
+fi
+
+echo "Importing chinook.sql ..."
+sqlite3 -batch data/chinook.db < db/chinook.sql
+
+echo "Done!"
